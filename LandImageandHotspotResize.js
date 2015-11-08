bdd56ac9ff648f848c899349643b5bb704a8750f
James Morgan <jmorgan@antennainternational.com>
2015-11-08 15:01:03 -0500

    resize for landscape
    first stab

---
+++ LandImageandHotspotResize.js
@@ -0,0 +1,38 @@
+//Landscape Calc
+
+
+imagewidth = 534;
+imageheight = 320;
+ImageRatio = imagewidth / imageheight;
+
+screenWidth = [[sys:devicePortraitHeight]];
+screenHeight = [[sys:devicePortraitWidth]];
+screenRatio = screenWidth / screenHeight;
+
+If ImageRatio = screenRatio {
+	newimagewidth = imagewidth;
+	newimageheight = imageheight;
+	hotspotMultiplier = [[sys:devicePortraitHeight]] / 534;
+}
+
+else {
+
+	ImageRatio > screenRatio {
+		newimagewidth = [[sys:devicePortraitHeight]];
+		hotspotMultiplier = [[sys:devicePortraitHeight]] / 534;
+		newimageheight = hotspotMultiplier * imageheight;
+		}
+
+	}
+
+	else {
+		newimageheight = [[sys:devicePortraitWidth]];
+		hotspotMultiplier = [[sys:devicePortraitWidth]] / 320;
+		newimagewith = hotspotMultiplier * imagewidth;
+	}
+
+}
+
+
+
+
